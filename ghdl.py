55079460e7488ed7f0a26c3c5a460cc4d5731bf7
FoFabien <f.fabien@hotmail.fr>
2019-09-17 17:11:25 +0000

    lot of improvements + setting saving

--- ghdl.py
+++ ghdl.py
@@ -1,13 +1,48 @@
 ﻿import os
 import sys
+import json
+import signal
 
 class GHDL():
     def __init__(self):
-        self.lastfile = None
-        self.lastentity = None
-        self.path = "ghdl-0.36\\bin\\ghdl.exe"
+        self.ghdlpath = "ghdl-0.36\\bin\\ghdl.exe"
         self.workpath = "workspace"
         self.current = None
+        self.base = os.getcwd()
+
+        signal.signal(signal.SIGINT, self.sigint)
+        self.load()
+
+    def sigint(self, sig, frame):
+        print('\n')
+
+    def load(self): # same thing but for save.json
+        try:
+            with open('settings.json') as f:
+                data = json.load(f)
+                if 'ghdl_path' in data: self.ghdlpath = data['ghdl_path']
+                if 'work_path' in data: self.workpath = data['work_path']
+                if 'last_project' in data:
+                    if self.isDir(self.workpath + "\\" + data['last_project']):
+                        self.current = data['last_project']
+                return True
+        except Exception as e:
+            print('load() Error:', e)
+            return False
+        return False
+
+    def save(self, sortBackup=True): # saving
+        try:
+            with open('settings.json', 'w') as f:
+                data = {}
+                data['ghdl_path'] = self.ghdlpath
+                data['work_path'] = self.workpath
+                data['last_project'] = self.current
+                json.dump(data, f)
+            return True
+        except Exception as e:
+            print('save() Error:', e)
+            return False
 
         if not self.isDir(self.workpath):
             os.mkdir(self.workpath)
@@ -45,8 +80,12 @@ class GHDL():
             print("Invalid choice, try again: ")
 
     def exe(self, cmd):
+        self.projectIn()
+        self.workDirCheck('work')
+        cmd = self.getExeStr() + " " + cmd
         print(">" + cmd)
         os.system(cmd)
+        self.projectOut()
 
     def getDir(self):
         files = os.listdir(self.workpath)
@@ -58,10 +97,28 @@ class GHDL():
         return res
 
     def isDir(self, folder):
-        return os.path.isdir(folder)
+        try:
+            return os.path.isdir(folder)
+        except:
+            return False
 
     def workDir(self):
         return self.workpath + "\\" + self.current
+
+    def workDirCheck(self, folder):
+        if not self.isDir(folder): 
+            os.mkdir(folder)
+
+    def projectIn(self):
+        os.chdir(self.workpath + "\\" + self.current)
+
+    def projectOut(self):
+        os.chdir(self.base)
+
+    def getExeStr(self):
+        exe = self.base
+        if not exe.endswith("\\"): exe = exe + "\\"
+        return "\"" + exe + self.ghdlpath + "\""
 
     def checkInputMenu(self, s):
         if s == "0":
@@ -107,39 +164,40 @@ class GHDL():
                     f = input("Input the file to compile: ")
                     if f == "":
                         continue
-                    self.exe(self.path + " -a --workdir=\"" + self.workDir() + "\" " + self.workDir() + "\\" + f)
+                    self.exe("-a --workdir=work " + f)
                 elif s == "3":
                     e = input("Input the entity to build: ")
-                    self.exe(self.path + " -e --workdir=\"" + self.workDir() + "\" "+ e)
+                    self.exe("-e --workdir=work "+ e)
                 elif s == "4":
-                    r = input("Input the entity to run: ")
-                    if r == "": continue
+                    e = input("Input the entity to run: ")
+                    if e == "": continue
                     a = input("Parameter(s)? : ")
-                    self.exe(self.path + " -r --workdir=\"" + self.workDir() + "\" " + r + "\" " + a)
+                    self.exe("-r --workdir=work " + e + " " + a)
                 elif s == "5":
                     e = input("Input the entity to build and run: ")
                     if e == "": continue
-                    self.exe(self.path + " -e --workdir=\"" + self.workDir() + "\" " + e)
+                    self.exe("-e --workdir=work " + e)
                     a = input("Parameter(s)? : ")
-                    self.exe(self.path + " -r --workdir=\"" + self.workDir() + "\" " + e + "\" " + a)
+                    self.exe("-r --workdir=work " + e + " " + a)
                 elif s == "6":
                     f = input("Input the file(s) to import: ")
                     if f == "": continue
-                    self.exe(self.path + " -i --workdir=\"" + self.workDir() + "\" " + self.workDir() + "\\" + f)
+                    self.exe("-i --workdir=work " + f)
                 elif s == "7":
                     m = input("Input the entity to make: ")
                     if m == "": continue
-                    self.exe(self.path + " -m --workdir=\"" + self.workDir() + "\" " + m)
+                    self.exe("-m --workdir=work " + m)
                 elif s == "8":
-                    self.exe(self.path + " --clean --workdir=\"" + self.workDir() + "\"")
+                    self.exe("--clean --workdir=work")
                 else: # quit
                     return
 
     def start(self):
         # we start HERE
-        print("GHDL quick use script v0.2")
+        print("GHDL quick use script v0.3")
         while self.loop(): # loop as long as loop() returns true
             pass
 
 g = GHDL()
 g.start()
+g.save()
