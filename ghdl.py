ca3344d91843cc9b0f133f62eb210d0f9a4c3015
FoFabien <f.fabien@hotmail.fr>
2019-09-17 15:42:49 +0000

    slight upgrade, more planned

--- ghdl.py
+++ ghdl.py
@@ -6,6 +6,11 @@ class GHDL():
         self.lastfile = None
         self.lastentity = None
         self.path = "ghdl-0.36\\bin\\ghdl.exe"
+        self.workpath = "workspace"
+        self.current = None
+
+        if not self.isDir(self.workpath):
+            os.mkdir(self.workpath)
 
     def askQuestion(self, question):
         while True:
@@ -39,78 +44,100 @@ class GHDL():
                     return ch
             print("Invalid choice, try again: ")
 
-    def printlast(self):
-        msg = ""
-        if self.lastfile is not None:
-            msg = " (Last file: " + self.lastfile
-        if self.lastentity is not None:
-            if msg == "":
-                msg = " (Last entity: " + self.lastentity + ")"
-            else:
-                msg = ", Last entity: " + self.lastentity + ")"
-        elif msg != "":
-            return msg + ")"
-        return msg
-
     def exe(self, cmd):
         print(">" + cmd)
         os.system(cmd)
+
+    def getDir(self):
+        files = os.listdir(self.workpath)
+        res = []
+        for f in files:
+            if os.path.isdir(os.path.join(os.path.abspath(self.workpath), f)):
+                res.append(f)
+        res.sort()
+        return res
+
+    def isDir(self, folder):
+        return os.path.isdir(folder)
+
+    def workDir(self):
+        return self.workpath + "\\" + self.current
+
+    def checkInputMenu(self, s):
+        if s == "0":
+            d = input("Input the project name (Leave blank to cancel): ")
+            if d == "":
+                return True
+            if self.isDir(self.workpath + "\\" + d):
+                print("Folder '" + d + "' already exists")
+                return True
+            try:
+                os.mkdir(self.workpath + "\\" + d)
+                self.current = d
+                print("Folder '" + d + "' created and set")
+            except Exception as e:
+                print("Error:", e)
+        elif s == "1":
+            d = input("Input the project name (Leave blank to cancel): ")
+            if d == "":
+                return True
+            if self.isDir(self.workpath + "\\" + d):
+                self.current = d
+                print("Project '" + d + "' is set")
+                return True
+            else:
+                print("Folder '" + d + "' not found")
+        else:
+            return False
+        return True
 
     # main loop
     def loop(self):
         # MAIN MENU
         while True:
-            s = self.menu("\nMain menu" + self.printlast(), [["0", "Compile"], ["1", "Build"], ["2", "Run"], ["3", "Build and Run"], ["4", "Import"], ["5", "Make"], ["6", "Clean workspace"], ["Any", "Exit"]], False)
-            if s == "0":
-                f = input("Input the file to compile: ")
-                if f == "":
-                    if self.lastfile is None: continue
-                    f = self.lastfile
-                self.exe(self.path + " -a --workdir=work  \"" + f + "\"")
-                self.lastfile = f
-            elif s == "1":
-                e = input("Input the entity to build: ")
-                if e == "":
-                    if self.lastentity is None: continue
-                    e = self.lastentity
-                self.exe(self.path + " -e --workdir=work \"" + e + "\"")
-                self.lastentity = e
-            elif s == "2":
-                r = input("Input the entity to run: ")
-                if r == "":
-                    if self.lastentity is None: continue
-                    r = self.lastentity
-                a = input("Parameter(s)? : ")
-                self.exe(self.path + " -r --workdir=work \"" + r + "\" " + a)
-                self.lastentity = r
-            elif s == "3":
-                e = input("Input the entity to build and run: ")
-                if e == "":
-                    if self.lastentity is None: continue
-                    e = self.lastentity
-                self.exe(self.path + " -e --workdir=work \"" + e + "\"")
-                a = input("Parameter(s)? : ")
-                self.exe(self.path + " -r --workdir=work \"" + e + "\" " + a)
-                self.lastentity = e
-            elif s == "4":
-                f = input("Input the file(s) to import: ")
-                if f == "": continue
-                self.exe(self.path + " -i --workdir=work \"" + f + "\"")
-            elif s == "5":
-                m = input("Input the entity to make: ")
-                if m == "":
-                    if self.lastentity is None: continue
-                    m = self.lastentity
-                self.exe(self.path + " -m --workdir=work \"" + m + "\"")
-                self.lastentity = m
-            elif s == "6":
-                self.exe(self.path + " --clean --workdir=work")
-            else: # quit
-                return False
+            if self.current is None or not self.isDir(self.workpath + "\\" + self.current):
+                s = self.menu("\nMain menu", [["0", "New project"], ["1", "Select project"], ["Any", "Exit"]], False)
+                if not self.checkInputMenu(s):
+                    return
+            else:
+                s = self.menu("\nMain menu - Current Project: " + self.current, [["0", "New project"], ["1", "Select project"], ["2", "Compile File (-a)"], ["3", "Build Entity (-e)"], ["4", "Run Entity (-r)"], ["5", "Build and Run Entity (-e, -r)"], ["6", "Import (-i)"], ["7", "Make Entity (-m)"], ["8", "Clean Workspace (--clean)"], ["Any", "Exit"]], False)
+                if self.checkInputMenu(s):
+                    continue
+                elif s == "2":
+                    f = input("Input the file to compile: ")
+                    if f == "":
+                        continue
+                    self.exe(self.path + " -a --workdir=\"" + self.workDir() + "\" " + self.workDir() + "\\" + f)
+                elif s == "3":
+                    e = input("Input the entity to build: ")
+                    self.exe(self.path + " -e --workdir=\"" + self.workDir() + "\" "+ e)
+                elif s == "4":
+                    r = input("Input the entity to run: ")
+                    if r == "": continue
+                    a = input("Parameter(s)? : ")
+                    self.exe(self.path + " -r --workdir=\"" + self.workDir() + "\" " + r + "\" " + a)
+                elif s == "5":
+                    e = input("Input the entity to build and run: ")
+                    if e == "": continue
+                    self.exe(self.path + " -e --workdir=\"" + self.workDir() + "\" " + e)
+                    a = input("Parameter(s)? : ")
+                    self.exe(self.path + " -r --workdir=\"" + self.workDir() + "\" " + e + "\" " + a)
+                elif s == "6":
+                    f = input("Input the file(s) to import: ")
+                    if f == "": continue
+                    self.exe(self.path + " -i --workdir=\"" + self.workDir() + "\" " + self.workDir() + "\\" + f)
+                elif s == "7":
+                    m = input("Input the entity to make: ")
+                    if m == "": continue
+                    self.exe(self.path + " -m --workdir=\"" + self.workDir() + "\" " + m)
+                elif s == "8":
+                    self.exe(self.path + " --clean --workdir=\"" + self.workDir() + "\"")
+                else: # quit
+                    return
 
     def start(self):
         # we start HERE
-        print("GHDL quick use script v0.1")
+        print("GHDL quick use script v0.2")
         while self.loop(): # loop as long as loop() returns true
             pass
 
